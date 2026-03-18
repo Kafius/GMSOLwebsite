48eae477d6bfd67b63f53b30dd9ba0077745536d
LinovaKafius <dennis_deleon06@yahoo.ca>
2026-03-17 22:34:07 -0400

    fix: explicitly pass env vars to Keystatic handler

--- main/src/pages/api/keystatic/[...params].ts
+++ main/src/pages/api/keystatic/[...params].ts
@@ -2,5 +2,10 @@ export const prerender = false;
 import { makeHandler } from '@keystatic/astro/api';
 import keystaticConfig from '../../../../keystatic.config';
 
-export const all = makeHandler({ config: keystaticConfig });
+export const all = makeHandler({
+  config: keystaticConfig,
+  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: process.env.KEYSTATIC_SECRET,
+});
 export { all as GET, all as POST };
